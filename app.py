5c1eb30b438e8204442d5b604d64a2bf01f07e4e
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2025-04-14 20:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
